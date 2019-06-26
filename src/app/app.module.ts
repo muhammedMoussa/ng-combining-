1a19363da538da9d52b91c25207fdace0b93c604
Muhammed <muhammedmoussa@hotmail.com>
2019-06-26 12:39:16 +0200

    add shared routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { App1SharedModule } from 'projects/app1/src/app/app.module';
 import { App2SharedModule } from 'projects/app2/src/app/app.module';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
