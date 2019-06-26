ae2e2fd0247ba294743174301c180ab155379915
Muhammed <muhammedmoussa@hotmail.com>
2019-06-26 12:25:56 +0200

    config app2 shared routes

--- projects/app2/src/app/app.module.ts
+++ projects/app2/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ModuleWithProviders } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { View1Component } from './view1/view1.component';
+import { View2Component } from './view2/view2.component';
+import { NavComponent } from './nav/nav.component';
+import { AppRoutingModule } from './app-routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    View1Component,
+    View2Component,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
